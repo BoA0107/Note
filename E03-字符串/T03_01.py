e020c7d41dbd16da20fb3c1190ecbba316a33d1d
boa0107 <2446912060@qq.com>
2021-06-03 16:48:51 +0800

    aaa

---
+++ E03-字符串/T03_01.py
@@ -0,0 +1,4 @@
+from math import *
+print('{0:-.2}\n{1:-.2}'.format(pi,-pi))
+
+
--- E04-字典/T04_01.py
+++ E04-字典/T04_01.py
@@ -1,4 +1,4 @@
 phonebook = {'Beth': '9102', 'Alice': '2341', 'Cecil': '3258'}
-"Cecil's phone number is {Cecil}".format_map(phonebook)
+a = "Cecil's phone number is {Cecil}".format_map(phonebook)
 
 print(a)
